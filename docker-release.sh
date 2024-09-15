8c4a4a9f7656dfbaa30d2c013c3206a04fc52da1
livecloud-labs <livecloud.store@gmail.com>
2024-09-15 23:10:17 +0100

    chore: add offline platform config

---
+++ docker-release.sh
@@ -0,0 +1,9 @@
+# Get version from package.json
+VERSION=$(cat package.json | grep '"version":' | awk -F'"' '{print $4}')
+
+# Tag the image with both 'latest' and the specific version number
+docker build -t relaybox/presence:latest -t relaybox/presence:$VERSION .
+
+# Push both the 'latest' and versioned tags to Docker Hub
+# docker push relaybox/presence:latest
+# docker push relaybox/presence:$VERSION
