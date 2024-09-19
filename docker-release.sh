fe9ada85b4e130e867528706f2c3a60b392001a5
livecloud-labs <livecloud.store@gmail.com>
2024-09-19 11:41:36 +0100

    chore: minor update

--- docker-release.sh
+++ docker-release.sh
@@ -5,5 +5,5 @@ VERSION=$(cat package.json | grep '"version":' | awk -F'"' '{print $4}')
 docker build -t relaybox/presence:latest -t relaybox/presence:$VERSION .
 
 # Push both the 'latest' and versioned tags to Docker Hub
-docker push relaybox/presence:latest
-docker push relaybox/presence:$VERSION
+# docker push relaybox/presence:latest
+# docker push relaybox/presence:$VERSION
